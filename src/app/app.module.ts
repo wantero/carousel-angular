7ae2a096ee79fbc3dc2a3c1ef130bcd972535746
William Pavei Antero <williamantero@gmail.com>
2019-09-17 14:56:56 -0300

    first carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AlertModule } from '../../node_modules/ngx-bootstrap';
+import { CarouselModule } from '../../node_modules/ngx-bootstrap/carousel';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
